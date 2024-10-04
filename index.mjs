73920c3fe27d09573f21e282e1a53f8a80d61f99
thuraaung2493 <thuraaung2493@gmail.com>
2024-10-04 14:50:02 +0700

    main action

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import figlet from 'figlet';
 import makeController from './src/commands/make/makeController.mjs';
 import makeRepository from './src/commands/make/makeRepository.mjs';
 import makeValidateSchema from './src/commands/make/makeValidateSchema.mjs';
 import newApp from './src/commands/newApp.mjs';
+import { log } from './src/logs.mjs';
 
 const program = new Command();
 
@@ -14,7 +16,20 @@ program
   .description(
     'A powerful CLI tool for quickly scaffolding TypeScript-based Express applications, complete with built-in file management commands for streamlined development.'
   )
-  .usage('command [options] [name]');
+  .usage('command [options] [name]')
+  .action(() => {
+    log(
+      figlet.textSync('TS Express App', {
+        horizontalLayout: 'fitted',
+        width: 120,
+      })
+    );
+
+    log('npx ts-expressor new <name>', 'Create New App - ');
+    log('npx ts-expressor make:controller <name>', 'Create Controller - ');
+    log('npx ts-expressor make:repository <name>', 'Create Repository - ');
+    log('npx ts-expressor make:schema <name>', 'Create Validate Schema - ');
+  });
 
 program.addCommand(newApp());
 program.addCommand(makeController());
